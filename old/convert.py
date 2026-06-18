9ed04f658752f8113e1c206de48bf0e9c5e05314
Matéo Lorenté <lorentemat@gmail.com>
2026-06-18 16:04:36 +0200

    Remove unused files from tracking

--- old/convert.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-subprocess.run([
-    "ffmpeg", "-i", "video/IMG_7171.MOV",
-    "-vf", "scale=1280:-1",   # réduit à 1280px de large
-    "-crf", "23",              # compression (18=haute qualité, 28=plus compressé)
-    "video/IMG_7171.mp4"
-])
